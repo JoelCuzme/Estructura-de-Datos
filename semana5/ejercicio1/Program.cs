86724a1282f04c506d74e512b48ddd8c32c07f1d
JoelCuzme <mj.cuzmel@uea.edu.ec>
2025-06-28 10:25:43 -0500

    Add files via upload

---
+++ semana5/ejercicio1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic; 
+public class Asignaturas 
+{
+    public static void Main(string[] args)
+    {
+        List<string> asignaturas = new List<string>
+        {
+            "Ciudadania",
+            "Química",
+            "Historia",
+            "Lenguaje"
+        };
+
+        Console.WriteLine("Las asignaturas son:");
+
+        foreach (string asignatura in asignaturas)
+        {
+            Console.WriteLine(asignatura);
+        }
+
+        Console.ReadKey(); 
+    }
+}
