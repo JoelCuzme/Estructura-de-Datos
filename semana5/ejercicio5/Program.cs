8ff6bcc3d2f9b0a78290c2463d2def01c5c997bb
JoelCuzme <mj.cuzmel@uea.edu.ec>
2025-06-28 10:28:36 -0500

    Add files via upload

---
+++ semana5/ejercicio5/Program.cs
@@ -0,0 +1,19 @@
+﻿
+using System.Collections.Generic;
+using System.Linq;
+
+public class PreciosSimple
+{
+    public static void Main(string[] args)
+    {
+        List<int> precios = new List<int> { 50, 75, 46, 22, 80, 65, 8 };
+
+        int precioMin = precios.Min();
+        int precioMax = precios.Max();
+
+        Console.WriteLine($"El precio más barato es: {precioMin}");
+        Console.WriteLine($"El precio más caro es: {precioMax}");
+
+        Console.ReadKey();
+    }
+}
