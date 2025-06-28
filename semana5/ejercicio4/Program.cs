0db1bf9901e8bf00a4792ea8c1bf0c1d530e5680
JoelCuzme <mj.cuzmel@uea.edu.ec>
2025-06-28 10:27:29 -0500

    Add files via upload

---
+++ semana5/ejercicio4/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic; // Esto nos ayuda a usar cosas como los diccionarios
+using System.Linq; // Nos da herramientas geniales como .ToDictionary() y .Count()
+
+public class Contarvocales
+{
+    public static void Main(string[] args)
+    {
+        Console.WriteLine("Escribe una palabra:");
+        string palabra = Console.ReadLine().ToLower(); 
+
+        
+        Dictionary<char, int> conteoVocales = new Dictionary<char, int>()
+        {
+            {'a', 0}, {'e', 0}, {'i', 0}, {'o', 0}, {'u', 0}
+        };
+
+        foreach (char letra in palabra)
+        {
+            if (conteoVocales.ContainsKey(letra))
+            {
+                conteoVocales[letra]++;
+            }
+        }
+
+        Console.WriteLine("\nTus vocales:");
+        foreach (var par in conteoVocales)
+        {
+            Console.WriteLine($"La '{par.Key}' apareció {par.Value} vez.");
+        }
+
+        Console.ReadKey(); 
+    }
+}
