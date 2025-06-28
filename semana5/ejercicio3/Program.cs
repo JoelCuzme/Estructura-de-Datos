44c1dc57e9da291c342d2b3ae349e6469667e5f0
JoelCuzme <mj.cuzmel@uea.edu.ec>
2025-06-28 10:27:03 -0500

    Add files via upload

---
+++ semana5/ejercicio3/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Loteria
+{
+    public static void Main(string[] args)
+    {
+        List<int> numerosganadores = new List<int>();
+
+        Console.WriteLine("Ingresa los 6 números ganadores de la Lotería:");
+
+        for (int i = 0; i < 6; i++)
+        {
+            Console.Write($"Número {i + 1}: ");
+            int numero = int.Parse(Console.ReadLine()); 
+            numerosganadores.Add(numero);
+        }
+
+        numerosganadores.Sort();
+
+        Console.WriteLine("\nNúmeros Ganadores Ordenados:");
+        Console.WriteLine(string.Join(", ", numerosganadores));
+
+        Console.ReadKey();
+    }
+}
