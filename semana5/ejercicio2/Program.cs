d31463d29d903fca1f0ec8af297c2099d15946fc
JoelCuzme <mj.cuzmel@uea.edu.ec>
2025-06-28 10:26:34 -0500

    Add files via upload

---
+++ semana5/ejercicio2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+public class Numeros
+{
+    public static void Main(string[] args)
+    {
+        string result = string.Join(",", Enumerable.Range(1, 10).Reverse().Select(n => n.ToString()));
+
+        Console.WriteLine(result);
+        Console.ReadKey();
+    }
+}
